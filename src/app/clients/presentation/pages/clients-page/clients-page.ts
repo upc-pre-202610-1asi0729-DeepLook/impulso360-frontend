9345f808b951c97a65164b3555392d07592aff9e
Gaby04 <gt8263257@gmail.com>
2026-05-14 10:12:34 -0500

    feat(clients): add delete cliente button

--- src/app/clients/presentation/pages/clients-page/clients-page.ts
+++ src/app/clients/presentation/pages/clients-page/clients-page.ts
@@ -52,4 +52,13 @@ export class ClientsPageComponent implements OnInit {
   trackByClientId(index: number, client: Client): number {
     return client.id;
   }
+  deleteClient(id: number): void {
+    const confirmed = confirm('¿Seguro que deseas eliminar este cliente?');
+
+    if (!confirmed) {
+      return;
+    }
+
+    this.clientStore.deleteClient(id);
+  }
 }
