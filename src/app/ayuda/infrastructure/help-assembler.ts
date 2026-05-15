57f68fc603c5f0b3fc43f175a01fa08302ed698d
Elias Daniel Ramos Cerdan <u20201a277@upc.edu.pe>
2026-05-14 21:35:21 -0500

    feat: add help assembler

---
+++ src/app/ayuda/infrastructure/help-assembler.ts
@@ -0,0 +1,49 @@
+﻿import { HelpFaq } from '../domain/model/help-faq.entity';
+import { HelpGuide } from '../domain/model/help-guide.entity';
+import { TutorialStep } from '../domain/model/tutorial-step.entity';
+import { HelpFaqResource } from './help-faq.resource';
+import { HelpGuideResource } from './help-guide.resource';
+import { TutorialStepResource } from './tutorial-step.resource';
+
+export class HelpAssembler {
+    static toFaqEntity(resource: HelpFaqResource): HelpFaq {
+        return new HelpFaq(
+            resource.id,
+            resource.question,
+            resource.answer
+        );
+    }
+
+    static toFaqEntities(resources: HelpFaqResource[]): HelpFaq[] {
+        return resources.map((resource) => this.toFaqEntity(resource));
+    }
+
+    static toGuideEntity(resource: HelpGuideResource): HelpGuide {
+        return new HelpGuide(
+            resource.id,
+            resource.icon,
+            resource.title,
+            resource.description,
+            resource.route
+        );
+    }
+
+    static toGuideEntities(resources: HelpGuideResource[]): HelpGuide[] {
+        return resources.map((resource) => this.toGuideEntity(resource));
+    }
+
+    static toTutorialStepEntity(resource: TutorialStepResource): TutorialStep {
+        return new TutorialStep(
+            resource.id,
+            resource.number,
+            resource.title,
+            resource.description,
+            resource.status,
+            resource.route
+        );
+    }
+
+    static toTutorialStepEntities(resources: TutorialStepResource[]): TutorialStep[] {
+        return resources.map((resource) => this.toTutorialStepEntity(resource));
+    }
+}
