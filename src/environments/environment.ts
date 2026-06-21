460f2f49b58bb9cc107821b37b41ef893c7f1d0f
alejandro202312510 <u202312510@upc.edu.pe>
2026-06-20 20:08:32 -0500

    fix: disable mock mode in production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@
 export const environment = {
   production: true,
   baseUrl: 'http://34.176.216.15:3000',
-  mock: typeof window !== 'undefined' && window.location.hostname !== 'localhost' && window.location.hostname !== '127.0.0.1'
+  mock: false
 };
 
